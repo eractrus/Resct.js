94a63744107d4e2a9176121c8fe707daef59d9b5
Eractrus de Judá <eractrus@gmail.com>
2023-12-07 22:52:32 -0300

    Tela home
    Tela home statica criada

--- Projetos/TaskWeek/my-app-week/src/Views/Home/index.jsx
+++ Projetos/TaskWeek/my-app-week/src/Views/Home/index.jsx
@@ -1,24 +1,101 @@
-
-import { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
+import './style.scss';
 
 function Home() {
-
-    const [usuario, setUsuario] = useState('')
+    const [usuario, setUsuario] = useState('');
 
     useEffect(() => {
-        const getNome = localStorage.getItem('@Usuario')
+        const getNome = localStorage.getItem('@Usuario');
 
         if (!getNome) {
-            window.localStorage.href='/'
+            // Redirect to the login page if no name is found in localStorage
+            window.location.href = '/';
         } else {
-
-            setUsuario(getNome)
+            setUsuario(getNome);
         }
-    }, [])
+    }, []);
 
     return (
-        <h1>Bem-vindo {usuario}</h1>
-    )
+        <div className="container">
+            <header className="containerHeader">
+                <h1>Bem-vindo {usuario}</h1>
+                <span>Seus compromissos</span>
+            </header>
+
+            <main className="containerMain">
+                <ul>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Segunda-feira</p>
+                                <span>0</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Terça-feira</p>
+                                <span>1</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Quarta-feira</p>
+                                <span>5</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Quinta-feira</p>
+                                <span>0</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Sexta-feira</p>
+                                <span>3</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Sábado</p>
+                                <span>15</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Domingo</p>
+                                <span>10</span>
+                            </div>
+                        </button>
+                    </li>
+                    <li>
+                        <button>
+                            <div className="card">
+                                <p>Extra</p>
+                                <span>+</span>
+                            </div>
+                        </button>
+                    </li>
+                </ul>
+            </main>
+
+            <nav className="containerNav">
+                <button>+</button>
+            </nav>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
--- Projetos/TaskWeek/my-app-week/src/Views/Splash/index.jsx
+++ Projetos/TaskWeek/my-app-week/src/Views/Splash/index.jsx
@@ -1,48 +1,45 @@
-
 import './style.scss';
 import { SlLayers } from "react-icons/sl";
 import { useState, useEffect } from 'react';
 
 function PageLogin() {
-
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
 
   useEffect(() => {
-
-    const getUsuario = localStorage.getItem('@Usuario')
+    const getUsuario = localStorage.getItem('@Usuario');
 
     if (getUsuario) {
-      window.location.href = `/Home`
+      // Se já houver um nome no localStorage, redirecione para a página /home
+      window.location.href = `/Home`;
     }
-  }, [])
+  }, []);
 
   function ColetarNome() {
-
     if (!name) {
-      alert('Insira seu nome')
-      return
+      alert('Insira seu nome');
+      return;
     }
 
-    localStorage.setItem('@Usuario', name)
-    window.location.href = `/Home`
-    
-
+    localStorage.setItem('@Usuario', name);
+    window.location.href = `/Home`;
   }
 
   return (
     <div className="App">
-
       <section className='logo'>
         <h1>TASKWEEK</h1>
         <SlLayers className='iconLogo' />
       </section>
 
       <input
-        onChange={e => { setName(e.target.value) }}
-        type="text" placeholder='Digite seu nome' />
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+        type="text"
+        placeholder='Digite seu nome'
+      />
 
-      <button
-        onClick={() => ColetarNome()}> Acessas </button>
+      <button onClick={() => ColetarNome()}> Acessar </button>
     </div>
   );
 }
